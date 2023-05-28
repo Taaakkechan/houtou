c610c0a1f3df315d3105a0df2c51a430c6bdffab
Jun <jun@Hillarys-MacBook-Pro.local>
2023-05-27 20:54:31 -0500

    added enemy bullets

--- app/client.ts
+++ app/client.ts
@@ -3,14 +3,25 @@ import { initializeGame } from 'app/initialize';
 
 initializeGame();
 
+interface Bullet {
+    x: number
+    y: number
+}
+
 const state = {
     horizontal: 400,
     vertical: 600,
-    isADown: false
-    isDDown: false
-    isWDown: false
-    isSDown: false
+    isADown: false,
+    isDDown: false,
+    isWDown: false,
+    isSDown: false,
+    EnemyX: 400,
+    EnemyY: 200,
+    bullets: [] as Bullet[],
+    cooldown: 0,
 };
+// @ts-ignore
+window['state'] = state;
 
 function update(): void {
     if (state.isADown) {
@@ -25,7 +36,35 @@ function update(): void {
     if (state.isSDown) {
         state.vertical = state.vertical + 10;
     }
+
+    for (let i = 0; i < state.bullets.length; i++){
+
+        state.bullets[i].y = state.bullets[i].y + 10;
+    }
+
+
+   if (state.cooldown <= 0) {
+        addBullet();
+        state.cooldown = 25;
+   }else {
+        state.cooldown --;
+   }
+
+    // Delete bullets that go off of the screen.
+    state.bullets = state.bullets.filter(b => 
+        b.y > -100 && b.y < 900 && b.x > -100 && b.x < 900
+    );
 }
+
+function addBullet(){
+
+     state.bullets.push({
+        x: 400,
+        y: 200,
+    });
+}
+
+//setInterval(addBullet, 1000);
 
 function render(context: CanvasRenderingContext2D): void {
     
@@ -33,12 +72,28 @@ function render(context: CanvasRenderingContext2D): void {
     context.fillStyle = 'black';
     context.fillRect(0, 0, 800, 800);
     
+     // Draw enemy
+    context.beginPath();
+    context.arc(state.EnemyX, state.EnemyY, 30, 0, 2*Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    
+    //Draw Bullets
+    for (let i = 0; i < state.bullets.length; i++){
+       
+        context.beginPath();
+        context.arc(state.bullets[i].x, state.bullets[i].y, 5, 0, 2*Math.PI);
+        context.fillStyle = 'white';
+        context.fill();
+    }
+   
     // Draw character
     context.beginPath();
     context.arc(state.horizontal, state.vertical, 20, 0, 2*Math.PI);
     context.fillStyle = 'yellow';
     context.fill();
 
+   
 }
 
 function renderLoop(): void {
